dbb54e3cfd95943a5f15afa02ded199a605aac71
Michael Tanczos <mike@logic-gate.com>
2024-03-16 20:07:30 -0400

    refactor: simplify action context configuration and bump version
    Removed the InvalidOperationException check in the ConfigureActionContext method to simplify the action context configuration process. This change reduces unnecessary complexity and improves code readability. Also, updated the project version from 1.1.4 to 1.1.5 in preparation for a new release.

--- src/Rizzy/Framework/Services/RzViewContext.cs
+++ src/Rizzy/Framework/Services/RzViewContext.cs
@@ -18,13 +18,9 @@ public class RzViewContext(IHttpContextAccessor httpContextAccessor, IUrlHelper 
     /// Configures the action context for the view context.
     /// </summary>
     /// <param name="actionContext">The action context to configure.</param>
-    /// <exception cref="InvalidOperationException">Thrown if the view context is already configured with an action context.</exception>
     /// <exception cref="ArgumentNullException">Thrown if the action context is null.</exception>
     internal void ConfigureActionContext(ActionContext actionContext)
     {
-        if (ActionContext != default(ActionContext))
-            throw new InvalidOperationException("RzViewContext has already been configured with an ActionContext");
-
         ArgumentNullException.ThrowIfNull(actionContext);
         ActionContext = actionContext;
     }
