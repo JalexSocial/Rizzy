a00011d85764f8a5d8901a171fad6de68de9f4a0
Michael Tanczos <mike@logic-gate.com>
2025-11-07 14:38:39 -0500

    feat: transfers MVC model state
    Transfers MVC model state errors to the Razor component's EditContext.
    
    Enables seamless validation integration between MVC and Razor components.
    
    Ensures validation messages are displayed correctly in the component.

--- src/Rizzy/Components/Form/RzInitialValidator.cs
+++ src/Rizzy/Components/Form/RzInitialValidator.cs
@@ -16,32 +16,10 @@ public class RzInitialValidator : ComponentBase
     public EditContext? EditContext { get; set; }
 
     /// <summary>
-    /// Gets or sets the ModelStateDictionary for this component.
-    /// </summary>
-    [CascadingParameter]
-    public ModelStateDictionary? ModelState { get; set; }
-
-    [Inject] private IServiceProvider ServiceProvider { get; set; } = default!;
-
-    /// <summary>
     /// Invoked when the component has received parameters from its parent.
     /// </summary>
     protected override void OnParametersSet()
     {
-        if (ModelState != null && EditContext != null)
-        {
-            var messages = new ValidationMessageStore(EditContext);
-
-            foreach (var error in ModelState.SelectMany(s => s.Value?.Errors.Select(e => (s.Key, e.ErrorMessage)) ?? Enumerable.Empty<(string, string)>()))
-            {
-                messages.Add(EditContext.Field(error.Item1), error.Item2);
-            }
-
-            EditContext.NotifyValidationStateChanged();
-        }
-        else
-        {
-            EditContext?.Validate();
-        }
+        EditContext?.Validate();
     }
 }
--- src/Rizzy/Components/Form/RzModelStateProvider.cs
+++ src/Rizzy/Components/Form/RzModelStateProvider.cs
@@ -5,9 +5,9 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 namespace Rizzy;
 
 /// <summary>
-/// A validator component that ensures model state errors are added to the EditContext.
+/// A component that transfer MVC model state errors to the Razor component
 /// </summary>
-public class RzInitialValidator : ComponentBase
+public class RzModelStateProvider : ComponentBase
 {
     /// <summary>
     /// Gets or sets the EditContext for this component.
@@ -16,10 +16,26 @@ public class RzInitialValidator : ComponentBase
     public EditContext? EditContext { get; set; }
 
     /// <summary>
+    /// Gets or sets the ModelStateDictionary for this component.
+    /// </summary>
+    [CascadingParameter]
+    public ModelStateDictionary? ModelState { get; set; }
+
+    /// <summary>
     /// Invoked when the component has received parameters from its parent.
     /// </summary>
     protected override void OnParametersSet()
     {
-        EditContext?.Validate();
+        if (ModelState != null && EditContext != null && !ModelState.IsValid)
+        {
+            var messages = new ValidationMessageStore(EditContext);
+
+            foreach (var error in ModelState.SelectMany(s => s.Value?.Errors.Select(e => (s.Key, e.ErrorMessage)) ?? Enumerable.Empty<(string, string)>()))
+            {
+                messages.Add(EditContext.Field(error.Item1), error.Item2);
+            }
+
+            EditContext.NotifyValidationStateChanged();
+        }
     }
 }
