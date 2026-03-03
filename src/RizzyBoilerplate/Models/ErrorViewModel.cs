8132dc8bcaaddbb89b2bb5e7decebd8f57a4aa9d
Michael Tanczos <mike@logic-gate.com>
2026-03-03 10:38:17 -0500

    chore: removed obsolete boilerplate folder

--- src/RizzyBoilerplate/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RizzyBoilerplate.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
